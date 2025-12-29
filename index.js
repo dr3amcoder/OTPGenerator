1abc91d03f237142c6c4ba940a1965e3b5033e38
dr3amcoder <jannalimpin@hotmail.co.uk>
2025-12-29 13:52:53 +0000

    Event Form RSVP App with styling

--- index.js
+++ index.js
@@ -184,6 +184,106 @@ const SuperheroForm = () => {
   )
 };
 
+
+const EventRSVPForm = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [number, setNumber] = useState("")
+  const [dietaryPreferences, setDietaryPreferences] = useState("")
+  const [isBringingPlusOne, setIsBringingPlusOne] = useState(false)
+  const [formSubmitted, setFormSubmitted] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newSubmittedData = {
+      name,
+      email,
+      number,
+      dietaryPreferences,
+      isBringingPlusOne
+    }
+
+    setFormSubmitted(newSubmittedData)
+
+  }
+
+
+  return(
+    <div className="container">
+        <h2 id="event-rsvp-title">Event RSVP</h2>
+        <form onSubmit={handleSubmit}>
+            <div className="event-form-details">
+                <label>
+                Name: 
+                <input 
+                    type="text"
+                    required
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+                </label>
+                <br />
+                <label>
+                Email: 
+                <input
+                    type="email"
+                    required 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                </label>
+                <br />
+                <label>
+                Number of attendees: 
+                <input
+                    type="number"
+                    required 
+                    value={number}
+                    onChange={e => setNumber(e.target.value)}
+                />
+                </label>
+                <br />
+                <label>
+                Dietary preferences: 
+                <input
+                    type="text"
+                    value={dietaryPreferences}
+                    onChange={e => setDietaryPreferences(e.target.value)}
+                />
+                </label>
+                <br />
+                <label>
+                Bringing additional guests?
+                <input
+                    type="checkbox"
+                    checked={isBringingPlusOne}
+                    onChange={e => setIsBringingPlusOne(e.target.checked)} 
+                />
+                </label>
+                <br />
+            </div>
+            <button
+                className='submit-btn'
+                type="submit"
+            >
+                Submit RSVP
+            </button>
+        </form>
+        {Object.keys(formSubmitted).length > 0 && (
+            <div className="rsvp-details">
+            <h3 id="event-rsvp-confirmation">RSVP Submitted!</h3>
+            <p>Name: {formSubmitted.name}</p>
+            <p>Email: {formSubmitted.email}</p>
+            <p>Number of attendees: {formSubmitted.number}</p>
+            <p>Dietary preferences: {formSubmitted.dietaryPreferences}</p>
+            <p>Bringing additional guests: {formSubmitted.isBringingPlusOne ? "Yes" : "No"}</p>
+            </div>
+        )}
+    </div>
+  )
+}
+
 const App = () => {
     return (
         <div className="app-wrapper">
@@ -191,6 +291,7 @@ const App = () => {
             <OTPGenerator />
             <CharacterCounter />
             <SuperheroForm />
+            <EventRSVPForm />
         </div>
     )
 };
